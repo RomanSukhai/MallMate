efec4ca22dd41fa03c389c4770c54280f8871ded
Roman Sukhai <romansukhai.com>
2023-10-11 07:19:35 +0300

    login BE

--- mallmate/forms.py
+++ mallmate/forms.py
@@ -11,3 +11,19 @@ class RegistrationForm(forms.ModelForm):
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                user = Person.objects.filter(email=email).first()
+            except Person.DoesNotExist:
+                raise forms.ValidationError("Емеіл не знайдений або він не правильний")
+
+            if user.password != password:
+                raise forms.ValidationError("Неправильний пароль.")
+
+        return cleaned_data
