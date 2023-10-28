2d2f8fb1169671659b3ba3e947dd9fdef90c90d7
Your Name <you@example.com>
2023-10-28 23:46:10 +0300

    trukytnuk_nahui

--- SUBD MySQL/db_connector.py
+++ SUBD MySQL/db_connector.py
@@ -6,7 +6,7 @@ def create_connection():
         connection = mysql.connector.connect(
             host='127.0.0.1',
             user='root',
-            password='namor2004',
+            password='root',
             database='mallmatedb'
         )
         if connection.is_connected():
--- mallmate/settings.py
+++ mallmate/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mallmatedb',
         'USER': 'root',
-        'PASSWORD': 'namor2004',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
