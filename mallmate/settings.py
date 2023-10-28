a820b9ecc30b07ea486a687b77134ea128733365
KLEPKO <denys.svintsylo.shi.2022@lpnu.ua>
2023-10-29 01:06:09 +0300

    my footer remake(piece of cake)

--- mallmate/settings.py
+++ mallmate/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mallmatedb',
         'USER': 'root',
-        'PASSWORD': 'namor2004',
+        'PASSWORD': 'messi2016',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
