2ef557708b70790e7d6ef5aac0e1ce2b76457a2c
Artem Duda <118640498+sesheii@users.noreply.github.com>
2023-11-05 20:13:37 +0200

    Update wsgi.py

--- mallmate/wsgi.py
+++ mallmate/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MallMate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mallmate.settings')
 
 application = get_wsgi_application()
