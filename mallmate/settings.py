2bd94cb3c7b5c12feff6f4e257541d8abf1ecb03
RomanSukhai <romansukhai@gmail.com>
2023-11-14 20:35:08 +0200

    Testing

--- mallmate/settings.py
+++ mallmate/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'mallmate.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default='mysql://localhost/mydb')
 }
 
 
