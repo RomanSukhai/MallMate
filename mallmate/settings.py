80fa06a6f3b077a765391f9308b09e9371c4f3c5
Artem Duda <118640498+sesheii@users.noreply.github.com>
2023-11-17 01:43:37 +0200

    Update settings.py

--- mallmate/settings.py
+++ mallmate/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'MallMate',
         'USER': 'root',
-        'PASSWORD': os.environ['MYSQL_PWD'],
+        'PASSWORD': 'namor2004',
         'HOST': 'localhost',
         'PORT': '3306',
     }
