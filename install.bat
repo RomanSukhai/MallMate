e54190a6da8faca63b38c1f16c5fc72116a9082d
Roman <romansukhai@gmail.com>
2023-11-17 11:19:03 +0200

    update install scripts

--- install.bat
+++ install.bat
@@ -1,9 +1,11 @@
 @echo off
+chcp 65001
 set /p MYSQL_PASSWORD=Введіть ваш пароль від MYSQL: 
 echo Ви ввели: %MYSQL_PASSWORD%
+set MYSQL_PWD=%MYSQL_PASSWORD%
 
-python -m virtualenv virtenv 
-.\virtenv\Scripts\activate 
+python -m venv virtenv 
+call virtenv\Scripts\activate
 pip install -r requirements.txt
 
 python manage.py makemigrations
--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 read -p "Введіть ваш пароль від MySQL: " MYSQL_PASSWORD
 echo "Ви ввели: $MYSQL_PASSWORD"
+export MYSQL_PWD=$MYSQL_PASSWORD
 
-python -m virtualenv virtenv
+python -m venv virtenv
 source virtenv/bin/activate
 
 pip install -r requirements.txt
