3613efd807221aea125d0d2c5c049a2f6531363c
FalconAI <61083677+bogdied@users.noreply.github.com>
2023-11-17 16:08:34 +0200

    improve install script

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 git clone -b mallmate_101_final_log_reg_0.3.1 https://github.com/RomanSukhai/MallMate.git
 cd .\MallMate\
 ```
-Якщо у вас windows, запустити файл install.bat в папці проекту. Воно все що треба встановить і запустить сервер
+Якщо у вас windows, __**З ПРАВАМИ АДМІНІСТРАТОРА**__ запустити файл install.bat в папці проекту. Воно все що треба встановить і запустить сервер
 
 
    ![cat](https://github.com/RomanSukhai/MallMate/assets/118640498/488761ad-a13a-438b-82df-02a00ef1bcc3)
--- install.bat
+++ install.bat
@@ -1,7 +1,20 @@
 @echo off
 cd /d "%~dp0"
 
-chcp 65001
+:admin_check
+    echo Потрібні права адміністратора. Перевірка прав...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Успіх: Права адміністратора підтверджено.
+    ) else (
+        echo Помилка: Поточні права недостатні.
+        pause >nul
+        exit /b 1
+    )
+
+
+@chcp 65001 >nul
 set /p MYSQL_PASSWORD=Введіть ваш пароль від MYSQL: 
 echo Ви ввели: %MYSQL_PASSWORD%
 setx MYSQL_PWD "%MYSQL_PASSWORD%" /M
