e3c2683d55206c30fdc843c5c9c4110ee9bb1ee6
RomanSukhai <romansukhai@gmail.com>
2023-12-10 10:55:52 +0200

    map-pictures

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -1,24 +1,47 @@
 $(document).ready(function(){
-    var map = L.map('map', {
-        attributionControl: false,
-        zoomControl: false,  
-        minZoom: 2,
-        maxZoom: 5,
-        center: [0, 0],
-        zoom: 1,
-        crs: L.CRS.Simple
-      });
-      var w = 3840,
-          h = 2160,
-          url = imageMapUrl;
-      
-      var southWest = map.unproject([0, h], map.getMaxZoom()-1);
-      var northEast = map.unproject([w, 0], map.getMaxZoom()-1);
-      var bounds = new L.LatLngBounds(southWest, northEast);
-      
-      L.imageOverlay(url, bounds).addTo(map);
-      
-      map.setMaxBounds(bounds);
+  var map = L.map('map', {
+    attributionControl: false,
+    zoomControl: false,  
+    minZoom: 1,
+    maxZoom: 1, // Збільшено maxZoom
+    center: [0, 0],
+    zoom: 5, 
+    crs: L.CRS.Simple
+  });
+  var w = 994,
+      h = 706;
+
+  var southWest = map.unproject([0, h], map.getMaxZoom());
+  var northEast = map.unproject([w, 0], map.getMaxZoom());
+  var bounds = new L.LatLngBounds(southWest, northEast);
+
+  map.setMaxBounds(bounds);
+
+  L.imageOverlay(src, bounds).addTo(map);
+
+  shops.forEach(function(shop) {
+    var pos = map.unproject([shop.coords[0], shop.coords[1]], map.getMaxZoom());
+    
+    var customHtml = `
+        <div style="text-align: center;">
+            <img src="${shop.iconUrl}" style="width: 38px; height: 38px;">
+            <div>${shop.text}</div>
+            <svg width="100" height="50">
+                <!-- Тут можна додати SVG код -->
+                <text x="10" y="25" fill="black">${shop.text}</text>
+            </svg>
+        </div>`;
+
+        var customIcon = L.icon({
+            html: customHtml,
+            iconUrl: shop.iconUrl,
+            iconSize: [shop.iconWidth, shop.iconHeight], // Розмір іконки для кожного магазину
+            iconAnchor: [shop.iconWidth / 2, shop.iconHeight], // Якір іконки
+            popupAnchor: [0, -shop.iconHeight / 2] // Якір вспливаючого вікна
+        });
+
+    L.marker(pos, {icon: customIcon}).addTo(map);
+});
 });
 
 $(document).ready(function(){
